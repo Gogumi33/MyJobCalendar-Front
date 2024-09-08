8128222fdf7b09a350f1075cd5ec573babf60cc2
Gogumi33 <135415213+Gogumi33@users.noreply.github.com>
2024-09-08 23:01:23 +0900

    http deploy

--- src/APIs/axiosInstance.js
+++ src/APIs/axiosInstance.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-//const API_URL = 'http://localhost:8080/';
+// const API_URL = 'http://localhost:8080/';
 
-const API_URL = 'https://mjcback.duckdns.org/';
+// const API_URL = 'https://mjcback.duckdns.org/';
+const API_URL = 'http://ec2-54-205-129-196.compute-1.amazonaws.com/';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
