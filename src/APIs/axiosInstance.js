9ab8e81b5fc9616e482e1313eaa0288cbfcd5f42
Gogumi33 <135415213+Gogumi33@users.noreply.github.com>
2024-09-13 15:08:24 +0900

    SSL certificate done

--- src/APIs/axiosInstance.js
+++ src/APIs/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // const API_URL = 'http://localhost:8080/';
 
 // const API_URL = 'https://mjcback.duckdns.org/';
-const API_URL = 'https://ec2-54-205-129-196.compute-1.amazonaws.com/';
+const API_URL = 'https://myjobcal.duckdns.org';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
