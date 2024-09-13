25b13b8bd0c9b19c692ea5f30ec789bbf11238f3
Gogumi33 <135415213+Gogumi33@users.noreply.github.com>
2024-09-13 16:48:15 +0900

    erase /

--- src/APIs/axiosInstance.js
+++ src/APIs/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // const API_URL = 'http://localhost:8080/';
 
 // const API_URL = 'https://mjcback.duckdns.org/';
-const API_URL = 'https://myjobcal.duckdns.org';
+const API_URL = 'https://myjobcal.duckdns.org'; // 뒤에 /는 제외시키는게 좋음.
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
